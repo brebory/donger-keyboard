3ad6af9d66513775742c8289d8b3cff16028444c
Brendon Roberto <Brendon.Roberto@onbase.com>
2016-06-23 17:54:54 -0400

    Add Donger keyboard target

---
+++ DongerKeyboard/DongerKit/DongerKit.h
@@ -0,0 +1,19 @@
+//
+//  DongerKit.h
+//  DongerKit
+//
+//  Created by Brendon Roberto on 6/23/16.
+//  Copyright © 2016 Snackpack Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DongerKit.
+FOUNDATION_EXPORT double DongerKitVersionNumber;
+
+//! Project version string for DongerKit.
+FOUNDATION_EXPORT const unsigned char DongerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DongerKit/PublicHeader.h>
+
+
